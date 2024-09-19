3c89fd21bfc758901292d613a62618c2d12e0f4c
dubya <dubya297@yahoo.com>
2024-09-18 19:13:39 -0500

    Finished the database setup script. use sudo mysql -u root < init_db.sql to setup the database schema.

--- init_db.sql
+++ init_db.sql
@@ -1,25 +1,54 @@
 DROP DATABASE IF EXISTS waves;
+CREATE DATABASE waves;
 use waves;
 
+CREATE TABLE tags(
+    id int,
+    tag varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE liked_clips(
+    id int,
+    clip_id int,
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE subscriptions(
+    id int,
+    subscription varchar(255),
+    PRIMARY KEY (id)
+);
+
 CREATE TABLE users (
-    PRIMARY KEY (id),
+    id int,
     username varchar(255) NOT NULL,
     password varchar(511) NOT NULL,
     description varchar(255),
-    FOREIGN KEY (subscriptions) REFERENCES users(username),
-    FOREIGN KEY (liked) REFERENCES LIKED_CLIPS(id)
+    subscriptions int,
+    liked int,
+    FOREIGN KEY (subscriptions) REFERENCES subscriptions(id),
+    FOREIGN KEY (liked) REFERENCES liked_clips(id),
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE clips (
-    PRIMARY KEY (id) NOT NULL,
-    views,
-    likes,
-    dislikes,
-    tags,
+    id int,
+    views int,
+    likes int,
+    dislikes int,
+    tags int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (tags) REFERENCES tags(id)
 );
 
-CREATE TABLE tags(
-    id,
-    tag,
+
+CREATE TABLE comments(
+    id int,
+    author int,
+    parent int,
+    FOREIGN KEY (author) REFERENCES users(id),
+    FOREIGN KEY (parent) REFERENCES comments(id),
+    PRIMARY KEY (id)
 );
 
