5a12d777d9dc519173759e5f9d76a039723ddd19
TrishaCarter <101431046+TrishaCarter@users.noreply.github.com>
2024-09-27 15:12:05 -0500

    started base files for home page

---
+++ html/home_page.php
@@ -0,0 +1,21 @@
+``` PHP File for Youtube Waves Home Page ```
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Youtube Waves</title>
+    </head>
+    <body>
+        <h1>Discover Your Sound</h1>
+    </body>
+
+    ``` Seach Bar ```
+    <form action="search.php" method="get">
+        <input type="text" name="search" placeholder="Search...">
+        <button type="submit">Search</button>
+
+    ``` profile icon ```
+    <a href="profile.php">
+        <img src="profile_icon.png" alt="Profile Icon">
+<html>
+    
