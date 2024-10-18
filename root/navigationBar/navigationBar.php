a5023628d391d610cd327b74aeccef6c6a2e71da
TrishaCarter <101431046+TrishaCarter@users.noreply.github.com>
2024-10-18 15:00:10 -0500

    navigation bar; start of landing page and settings page. Gonna wait on universal style sheet before finishing them.

---
+++ root/navigationBar/navigationBar.php
@@ -0,0 +1,41 @@
+<!-- navbar.php -->
+<nav>
+    <ul>
+        <li><a href="../landing_page/about.php">About</a></li>
+        <li><a href="../home_page/home_page.html">Discover</a></li>
+        <li><a href="../profile/profile.php">Profile</a></li>
+        <li><a href="../settings/settings.php">Settings</a></li>
+    </ul>
+</nav>
+
+<style>
+    nav {
+        position: fixed;
+        bottom: 0;
+        width: 100%;
+        background-color: var(--color-bg-secondary);
+        padding: 1em;
+        z-index: 1000;
+    }
+    ul {
+        list-style-type: none;
+        margin: 0;
+        padding: 0;
+        overflow: hidden;
+        text-align: center;
+    }
+    li {
+        display: inline-block;
+    }
+    li a {
+        display: inline-block;
+        color: #B0B0B0;
+        text-align: center;
+        padding: 14px 16px;
+        text-decoration: none;
+        pointer: cursor;
+    }
+    li a:hover {
+        background-color: var(--color-shadow);
+    }
+</style>
---
+++ root/settings/settings.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Settings</title>
+    <link rel="stylesheet" href="../root.css"/>
+    <link rel="stylesheet" href="../profile/profile.css"/>
+</head>
+<body>
+    <?php include '../navigationBar/navigationBar.php'; ?>
+</body>
