8a50082823f05e813613e43f4b9c0289e274bb67
TrishaCarter <101431046+TrishaCarter@users.noreply.github.com>
2024-10-23 14:24:49 -0500

    minor changes

--- root/landing_page/about.php
+++ root/landing_page/about.php
@@ -11,7 +11,7 @@
     
     <div id="landing-container">
         <h1 class="welcome-text">Welcome to YouTube Waves!</h1>
-        <p class="sub-text">Your go-to platform for sharing and discovering fun audios and soundbytes.</p>
+        <p class="sub-text">Your go-to platform for sharing and discovering fun audios..</p>
 
         <div id="button-container">
             <button class="landing-btn green-bg" onclick="window.location.href='../login/login.php'">Login</button>
