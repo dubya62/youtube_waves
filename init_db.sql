6b5f15a4d05995c22e21b1d64eeef0bbe9089f35
dubya <dubya297@yahoo.com>
2024-10-24 16:56:13 -0500

    The entire process of uploading a clip and it showing up on the Discover page is finished.

--- init_db.sql
+++ init_db.sql
@@ -16,6 +16,16 @@ CREATE TABLE users (
     PRIMARY KEY (id)
 );
 
+-- This table keeps track of which tags users like
+CREATE TABLE user_tags (
+    id int AUTO_INCREMENT,
+    user_id int,
+    tag_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
+);
+
 
 CREATE TABLE subscriptions(
     id int AUTO_INCREMENT,
@@ -31,11 +41,28 @@ CREATE TABLE clips (
     owner int, -- user's id
     name varchar(255),
     time datetime,
-    tags int,
     extension varchar(16),
+    image_extension varchar(16),
     PRIMARY KEY (id),
-    FOREIGN KEY (owner) REFERENCES users(id),
-    FOREIGN KEY (tags) REFERENCES tags(id)
+    FOREIGN KEY (owner) REFERENCES users(id)
+);
+
+CREATE TABLE watched_clips (
+    id int AUTO_INCREMENT,
+    user_id int,
+    clip_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id) REFERENCES users(id),
+    FOREIGN KEY (clip_id) REFERENCES clips(id)
+);
+
+CREATE TABLE clip_tags (
+    id int AUTO_INCREMENT,
+    clip_id int,
+    tag_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (clip_id) REFERENCES clips(id),
+    FOREIGN KEY (tag_id) REFERENCES tags(id)
 );
 
 CREATE TABLE liked_clips(
