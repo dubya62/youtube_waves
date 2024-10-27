42f8bc162712fb20fd85b316a672cac16f5f5906
dubya62 <118396384+dubya62@users.noreply.github.com>
2024-10-27 13:09:56 -0500

    Maybe didn't break home_page.php
    Removed the php script that loads additional clips. Should still load more clips since it is in the ajax call.

--- root/home_page/home_page.php
+++ root/home_page/home_page.php
@@ -253,76 +253,6 @@
     <center>Discover</center>
 </h1>
 <div class="audio-container">
-    <?php
-        include '../../includes/scripts.php';
-
-        function createClip($conn, $clip_id){
-            echo "<div class='audio-item' id='clip-" . $clip_id . "' onclick='openClipMenu(\"clip-" . $clip_id . "\")'>
-                <img src='images/" . $clip_id . "." . getImageExtension($conn, $clip_id) . "' alt='Thumbnail' class='thumbnail'>
-                <div class='audio-title'>" . getClipName($conn, $clip_id) . "</div>
-                <audio controls class='audio-player'>
-                    <source src='audios/" . $clip_id . "." . getClipExtension($conn, $clip_id) . "' type='audio/mp3'>
-                    Your browser does not support the audio element.
-                </audio>
-
-                <div class='button-container'>
-                    <!-- LIKE Button -->
-                    <button class='btn-23' onclick='incrementLike(\"like-counter-". $clip_id . "\")'>
-                        <span class='text'>LIKE</span>
-                        <span class='marquee'>LIKE</span>
-                    </button>
-                    <p id='like-counter-" . $clip_id . "'>0</p>  <!-- Like counter -->
-                    
-                    <!-- DISLIKE Button -->
-                    <button class='btn-23' onclick='incrementDislike(\"dislike-counter-" . $clip_id . "\")'>
-                        <span class='text'>DISLIKE</span>
-                        <span class='marquee'>DISLIKE</span>    
-                    </button>
-                    <p id='dislike-counter-" . $clip_id . "'>0</p> <!-- Dislike counter -->
-                </div>
-                
-                    <!-- COMMENT Button -->
-                    <div class='comment-container'>
-                        <button class='btn-23' onclick='openCommentPopup()'>
-                            <span class='text'>RANT</span>
-                            <span class='marquee'>RANT</span>
-                        </button>
-                
-                    </div>
-                        
-
-                </div>";
-
-        }
-
-        function createClips($conn, $clip_ids){
-
-            # create each clip
-            foreach ($clip_ids as $clip_id){
-                createClip($conn, $clip_id);
-            }
-
-        }
-
-        $currentClipNumber = 0;
-
-        function getNextClipBatch($batchSize, $currentClipNumber){
-            $conn = initDb();
-
-            # get a batch of clip_ids
-            $clip_ids = getClipBatch($conn, $currentClipNumber, $batchSize);
-
-            # display the clips on the home page
-            createClips($conn,  $clip_ids);
-
-            closeDB($conn);
-        }
-
-        # start with displaying a batch of at most 30 clips for now
-        getNextClipBatch(30, $currentClipNumber);
-        $currentClipNumber += 30;
-
-    ?>
 
     <!-- COMMENT Textbox Popup-->
     <div class="popup" id="comment-popup" onclick="event.stopPropagation()">
@@ -428,7 +358,6 @@
     <div id="popupForm" class="otherPopup">
         <div class="popup-content">
             <span class="close">&times;</span>
-            <form id="uploadForm">
             <form method="post" id="uploadForm" enctype="multipart/form-data" onsubmit="setTimeout(function {window.location.reload();}, 10);">
                 <h2 style="color: var(--color-text-primary)">Create New Post</h2>
                 <label for="name">Name:</label>
